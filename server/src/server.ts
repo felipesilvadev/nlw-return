ae8f5e36b942cdacee0c3e0226eedf4296dbdce1
Felipe Silva <felipe.silva@assesi.com>
2022-05-06 16:51:02 -0300

    fix: add server port env variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,8 +7,10 @@ import { routes } from './routes';
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json({ limit: '1mb' }))
+app.use(bodyParser.json({ limit: '1mb' }));
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log('HTTP server running!'));
+app.listen(process.env.PORT || 3333, () => {
+  console.log('HTTP server running!')
+});
